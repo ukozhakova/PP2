483057d5c24d25c5ed594f0045565711fc20e8a2
Ulbossyn <ulbossynkozhakova@gmail.com>
2018-01-17 13:48:10 +0600

    Week1

---
+++ Week1/Primes in main/Primes in main/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example2
+{
+	class Program
+	{
+		/// <summary>
+		/// Function for finding prime numbers
+		/// </summary>
+		/// <param name="x">possible prime number</param>
+		/// <returns>answer for question</returns>
+		static bool IsPrime(int x)
+		{
+			bool res = true;
+
+			if (x == 1) return false;
+
+			for (int i = 2; i <= Math.Sqrt(x); ++i)
+			{
+				if (x % i == 0)
+				{
+					res = false;
+					break;
+				}
+			}
+
+			return res;
+		}
+
+		static void Main(string[] args)
+		{
+			foreach (string s in args)
+			{
+				if (IsPrime(int.Parse(s)))
+				{
+					Console.WriteLine(s);
+				}
+			}
+		}
+	}
+}

9766631ca4d1f04cf213d2636e1e4c0baf006500
Ulbossyn <ulbossynkozhakova@gmail.com>
2018-01-26 17:19:16 +0600

    Lab

---
+++ Copy FIle/перемещать файлы/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace перемещать_файлы
+{
+	public class SimpleFileMove
+	{
+		static void Main()
+		{
+			string sourceFile = @"C:\Users\Asus\Desktop\family\test.txt";
+			string destinationFile = @"C:\Users\Asus\Desktop\Mamam\test.txt";
+
+			// To move a file or folder to a new location:
+			System.IO.File.Move(sourceFile, destinationFile);
+
+			// To move an entire directory. To programmatically modify or combine
+			// path strings, use the System.IO.Path class.
+			System.IO.Directory.Move(@"C:\Users\Asus\Desktop\family\prog Tomp\hh", @"C:\Users\Asus\Desktop\family\ConsoleApp1");
+		}
+	}
+}

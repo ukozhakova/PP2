bfd72a455ee26d699fb4fdb26b76850149d74b82
Your Name <you@example.com>
2018-01-29 15:28:48 +0600

    Labs

---
+++ Laboratory1/Primes in main/Primes in main/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Primes_in_main 
+{
+	class Program
+	{
+		/// <summary>
+		/// Function for checking is it prime
+		/// </summary>
+		/// <param name="x">possible prime number</param>
+		/// <returns></returns> 
+		static bool IsPrime(int x)
+		{
+			bool res = true;
+
+			if (x == 1) return false; //special case
+
+			for (int i = 2; i <= Math.Sqrt(x); ++i)
+			{
+				if (x % i == 0)
+				{
+					res = false; 
+					break; //если х делится на какое-то число, то дальше нет смысла проверять
+				}
+			}
+
+			return res; //вернет true or false
+		}
+
+		static void Main(string[] args)
+		{
+			foreach (string s in args)
+			{
+				if (IsPrime(int.Parse(s))) //проверяет на простоту
+				{
+					Console.WriteLine(s); //выводит простое число, иначе ниче не делает
+				}
+			}
+		}
+	}
+}
